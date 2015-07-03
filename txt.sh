e35175de9459c0b6865aa31739b531ab958e65cd
Xanthos Vlachos <root@localhost.localdomain>
2015-07-03 10:48:10 +0300

    txtcommand

---
+++ txt.sh
@@ -0,0 +1,1 @@
+sudo apt-get install zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libjasper-dev libopenexr-dev libgdal-dev libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev build-essential
